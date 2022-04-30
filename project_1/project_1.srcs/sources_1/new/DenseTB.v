abe74100f1c56a19f8c3931e8839707cd1feed14
Projects-Slayer <49372184+Projects-Slayer@users.noreply.github.com>
2022-04-30 03:16:11 +0200

    output until dense1

--- project_1/project_1.srcs/sources_1/new/DenseTB.v
+++ project_1/project_1.srcs/sources_1/new/DenseTB.v
@@ -1,17 +1,17 @@
 `timescale 1ns / 1ps
 
 
-module DenseTB#(parameter n_outputs = 128, n_inputs = 64, Input_size = 32);
-        wire [Input_size*n_inputs-1:0] in;
-        wire [n_outputs*n_inputs*Input_size-1:0] weights;
+module DenseTB#(parameter n_outputs = 64, n_inputs = 128, Input_size = 32);
+        wire [Input_size*n_inputs-1:0] ind1;
+        wire [n_outputs*n_inputs*Input_size-1:0] weights1;
         wire [n_outputs*(Input_size)-1:0] Out;
         
-        RAMD ramd(.in(in),.weights(weights));
+        RAMD ramd(.ind1(ind1),.weights1(weights1));
         
         DenseRelu #(.n_outputs(n_outputs),.n_inputs(n_inputs),.Input_size(Input_size)) u1(
-        .INBUS(in),
+        .INBUS(ind1),
         .OUTBUS(Out),
-        .weights(weights)
+        .weights(weights1)
 );
     initial begin
     #10;
--- project_1/project_1.srcs/sources_1/new/top1.v
+++ project_1/project_1.srcs/sources_1/new/top1.v
@@ -8,6 +8,12 @@ module top1#(parameter C_windows=8,parameter C_size=32,parameter C_filters=128,p
     wire [C2W*C2F*C_size-1:0] OutC2;
     wire [512*C_size-1 : 0]InC2;
     wire  [64*C_size-1 : 0]OutM2;
+    wire [128*C_size-1 : 0]OutDense;
+    wire [64*C_size-1 : 0]OutDense1;
+    wire [5*C_size-1 : 0]OutDense2;
+    wire [262143 : 0]weightsDense;
+    wire [262143 : 0]weightsDense1;
+    wire [10239 : 0]weightsDense2;
     EMB_Layer #(.size(C_windows+1))EMB (
     .INBUS('b100011001001100100100001011),
     .OUTBUS(data)
@@ -15,6 +21,7 @@ module top1#(parameter C_windows=8,parameter C_size=32,parameter C_filters=128,p
     
     RAM ram(.Outp(WT));
     Ram2 ram2(.Outp(WT2));
+    RAMD ramd(.weights(weightsDense),.weights1(weightsDense1),.weights2(weightsDense2));
     Conv_Layer#(.size(C_size),.n_inputs(C_n_inputs),.windows(C_windows),.filters(C_filters),.DP(DP)) C(
     .weights(WT),
     .data  (data),
@@ -40,15 +47,36 @@ module top1#(parameter C_windows=8,parameter C_size=32,parameter C_filters=128,p
         .INBUS(OutC2),
         .OUTBUS(OutM2)
         );
+    DenseRelu#(.n_outputs(128), .n_inputs(64), .Input_size(32))D
+            (
+                .INBUS(OutM2),
+                .weights(weightsDense),
+                .OUTBUS(OutDense)
+            );
+    DenseRelu#(.n_outputs(64), .n_inputs(128), .Input_size(32))D1
+                    (
+                        .INBUS(OutDense),
+                        .weights(weightsDense1),
+                        .OUTBUS(OutDense1)
+                    );
+    DenseRelu#(.n_outputs(5), .n_inputs(64), .Input_size(32))D2
+                    (
+                        .INBUS(OutDense1),
+                        .weights(weightsDense2),
+                        .OUTBUS(OutDense2)
+                    );                                
         
     
     initial begin
     #10
-    $display("%b",Out[C_size*20-1:0]);
-    $display("%b",OutM[C_size*20-1:0]);
-    $display("%b",InC2[C_size*135-1:0]);
-    $display("%b",OutC2[C_size*20-1:0]);
-    $display("%b",OutM2[C_size*20-1:0]);
+//    $display("%b",Out[C_size*20-1:0]);
+//    $display("%b",OutM[C_size*20-1:0]);
+//    $display("%b",InC2[C_size*135-1:0]);
+//    $display("%b",OutC2[C_size*20-1:0]);
+//    $display("%b",OutM2[C_size*20-1:0]);
+       $display("%b",OutDense);
+      $display("%b",OutDense1);
+      $display("%b",OutDense2);
     end
     
 endmodule
