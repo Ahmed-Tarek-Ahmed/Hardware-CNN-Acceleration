e687dcb7471b1043dd92e108bf66ef0dd681825c
Khaled Mostafa AbdelRazek <k.mostaf999@gmail.com>
2022-05-28 20:15:32 +0200

    new memory blocks

--- project_1/project_1.srcs/sources_1/imports/Conv1_Weights/Conv1_Weights_stub.vhdl
+++ project_1/project_1.srcs/sources_1/imports/Conv1_Weights/Conv1_Weights_stub.vhdl
@@ -1,31 +1,31 @@
 -- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2018.2 (win64) Build 2258646 Thu Jun 14 20:03:12 MDT 2018
--- Date        : Sat May 28 17:16:05 2022
+-- Date        : Sat May 28 17:12:27 2022
 -- Host        : DESKTOP-PCIMVFJ running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode synth_stub
---               d:/University/Graduation_Project/Hardware-CNN-Acceleration-/project_1/project_1.srcs/sources_1/ip/Conv2_Weights/Conv2_Weights_stub.vhdl
--- Design      : Conv2_Weights
+--               d:/University/Graduation_Project/Hardware-CNN-Acceleration-/project_1/project_1.srcs/sources_1/ip/Conv1_Weights/Conv1_Weights_stub.vhdl
+-- Design      : Conv1_Weights
 -- Purpose     : Stub declaration of top-level module interface
 -- Device      : xc7vx485tffg1157-1
 -- --------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity Conv2_Weights is
+entity Conv1_Weights is
   Port ( 
     clka : in STD_LOGIC;
-    addra : in STD_LOGIC_VECTOR ( 5 downto 0 );
+    addra : in STD_LOGIC_VECTOR ( 2 downto 0 );
     douta : out STD_LOGIC_VECTOR ( 4095 downto 0 )
   );
 
-end Conv2_Weights;
+end Conv1_Weights;
 
-architecture stub of Conv2_Weights is
+architecture stub of Conv1_Weights is
 attribute syn_black_box : boolean;
 attribute black_box_pad_pin : string;
 attribute syn_black_box of stub : architecture is true;
-attribute black_box_pad_pin of stub : architecture is "clka,addra[5:0],douta[4095:0]";
+attribute black_box_pad_pin of stub : architecture is "clka,addra[2:0],douta[4095:0]";
 attribute x_core_info : string;
 attribute x_core_info of stub : architecture is "blk_mem_gen_v8_4_1,Vivado 2018.2";
 begin
